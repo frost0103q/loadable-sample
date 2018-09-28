730321deec66d8f93b63a5da98a05707319ad006
velopert <public.velopert@gmail.com>
2018-09-29 04:08:38 +0900

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Loadable from 'react-loadable';
+import withSplitting from '../withSplitting';
+
+const Loading = () => {
+  return <div>로딩중...</div>;
+};
+
+export const Home = Loadable({
+  loader: () => import('./Home'),
+  loading: Loading
+});
+// export const About = Loadable({
+//   loader: () => import('./About'),
+//   loading: Loading
+// });
+
+export const About = withSplitting(() => import('./About'));
